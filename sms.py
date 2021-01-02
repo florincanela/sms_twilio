72186ce53d52e59a8ad417e6bc484a29fd1c453b
Florin Canela <florincanela@gmail.com>
2021-01-02 19:00:39 +0200

    2nd commit

--- sms.py
+++ sms.py
@@ -7,12 +7,12 @@ receiver = argv[1]
 with open('sms_body.txt', 'r') as s:
 	sms_body = s.read()
  
-account_sid = 'AC77607503b9681527a34b003573dc3cf6' 
-auth_token = '3181d5fdf3028d57151d1c38ada00291' 
+account_sid = #sid 
+auth_token = #auth token 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create(
-   							  from_='+12566854801',
+   							  from_=#twilio number,
                               body=sms_body,      
                               to=receiver 
                           ) 
